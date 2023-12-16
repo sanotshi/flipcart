8f07cb6e6f6030b8f83086d40cc2fedd430b5de8
santoshi <santoshitolapi2000@gmail.com>
2023-12-16 18:38:49 +0530

    Install tailwind into app

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <div>
+            <img src="https://static-assets-web.flixcart.com/batman-returns/batman-returns/p/images/fkheaderlogo_exploreplus-44005d.svg" alt="icon" />
+        </div>
+    </div>
+  )
+}
+
+export default Header
